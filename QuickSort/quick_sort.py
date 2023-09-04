1e01963f37fe00e85abde2eb30a164c9d2086516
Leraner <legend.tony@yandex.ru>
2023-09-04 13:39:21 +0300

    Average and worst case in QuickSort

--- QuickSort/quick_sort.py
+++ QuickSort/quick_sort.py
@@ -4,18 +4,50 @@
 второй - элементы больше опорного, к ним применяется тот же алгоритм быстрой сортировки
 Занимает достаточно большое кол-во памяти
 O-notation - n*log(n)
+
+Если берем первый элемент, затрачивается много памяти из-за стека вызовов, потому что он всегда держит массив []
+Лучшего случая не бывает, бывает худший и средний, потому что лучший случай - средний
+Даже если брать рандомный элемент всегда будет средний случай
 """
+import random
 
 
-def quick_sort(arr):
+def quick_sort1(arr):
     if len(arr) < 2:
         return arr
     else:
         support_element = arr[0]
-        less_than_support_element = [i for i in arr[1::] if i <= support_element]
-        greate = [i for i in arr[1::] if i > support_element]
+        less_than_support_element = [i for i in arr if i <= support_element]
+        greate = [i for i in arr if i > support_element]
 
-        return quick_sort(less_than_support_element) + [support_element] + quick_sort(greate)
+        return quick_sort1(less_than_support_element) + [support_element] + quick_sort1(greate)
 
 
-print(quick_sort([1, 10, 2, 50, 3, 2]))
+def quick_sort2(arr):
+    if len(arr) < 2:
+        return arr
+    else:
+        low = 0
+        high = len(arr) - 1
+        support_element = arr[(low + high) // 2]
+        arr.pop((low + high) // 2)
+        less_than_support_element = [i for i in arr if i <= support_element]
+        greate = [i for i in arr if i > support_element]
+
+        return quick_sort2(less_than_support_element) + [support_element] + quick_sort2(greate)
+
+
+def quick_sort3(arr):
+    if len(arr) < 2:
+        return arr
+    else:
+        index = random.randint(0, len(arr) - 1)
+        support_element = arr[index]
+        arr.pop(index)
+        less_than_support_element = [i for i in arr if i <= support_element]
+        greate = [i for i in arr if i > support_element]
+
+        return quick_sort2(less_than_support_element) + [support_element] + quick_sort2(greate)
+
+
+print(quick_sort3([1, 5, 3, 2]))
